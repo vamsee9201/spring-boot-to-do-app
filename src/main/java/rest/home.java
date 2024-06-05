6e9eae29ec6fa27367dfac889e8fc45d14e979f4
vamsee9201 <vamseekrishna9201@gmail.com>
2024-06-04 22:48:21 -0700

    adding get mapping

--- src/main/java/rest/home.java
+++ src/main/java/rest/home.java
@@ -1,4 +1,10 @@
 package rest;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@GetMapping("/helloworld")
 public class home {
+    return "hello world!";
 }
