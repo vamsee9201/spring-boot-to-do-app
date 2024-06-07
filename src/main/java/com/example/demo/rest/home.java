b0a6f573df713a7958c1129dd754d0a3b3d63433
vamsee9201 <vamseekrishna9201@gmail.com>
2024-06-06 22:30:15 -0700

    added new code

--- src/main/java/com/example/demo/rest/home.java
+++ src/main/java/com/example/demo/rest/home.java
@@ -1,14 +1,16 @@
-package rest;
+package com.example.demo.rest;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Component
 @RestController
 public class home {
     @GetMapping("/helloworld")
-    public String helloWorld() {
-        return "hello world!";
+    public String hello() {
+        return "hello, world";
     }
+
+    //@GetMapping("")
+
 }
+
